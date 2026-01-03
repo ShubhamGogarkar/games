9fb7df3bf1964116111d10eb442929bb8836f150
ShubhamGogarkar <shubhamgogarkar22@gmail.com>
2026-01-03 17:58:21 +0530

    remove color changing feature from main

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -38,16 +38,16 @@ function showComputer(choice) {
 }
 
 
-function getRandomColor() {
-    let val1= Math.floor(Math.random()*256);
-    let val2 = Math.floor(Math.random()*256);
-let val3 = Math.floor(Math.random()*256);
-return `rgb(${val1}, ${val2}, ${val3})`;
-}
-setInterval(()=>{document.querySelector("body").style.backgroundColor=getRandomColor()},1000);
-setInterval(()=>{document.querySelector("nav").style.backgroundColor=getRandomColor()},1000);
-setInterval(()=>{document.querySelector(".choiceandscore").style.backgroundColor=getRandomColor()},1000);
-setInterval(()=>{document.querySelector(".message").style.backgroundColor=getRandomColor()},1000);
+// function getRandomColor() {
+//     let val1= Math.floor(Math.random()*256);
+//     let val2 = Math.floor(Math.random()*256);
+// let val3 = Math.floor(Math.random()*256);
+// return `rgb(${val1}, ${val2}, ${val3})`;
+// }
+// setInterval(()=>{document.querySelector("body").style.backgroundColor=getRandomColor()},1000);
+// setInterval(()=>{document.querySelector("nav").style.backgroundColor=getRandomColor()},1000);
+// setInterval(()=>{document.querySelector(".choiceandscore").style.backgroundColor=getRandomColor()},1000);
+// setInterval(()=>{document.querySelector(".message").style.backgroundColor=getRandomColor()},1000);
 
 
 
